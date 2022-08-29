dece5ee072b71d22f2e60970494af57867bee8c2
nxoo <nxoo.dev@gmail.com>
2022-08-29 09:35:09 +0300

    learn-lab doesn't work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    document.querySelector('form').addEventListener('submit',(e)=>{
+        e.preventDefault()
+        handleToDo(e.target.new_task.value)
+    })
+})
+
+function handleToDo(todo){
+    let li = document.createElement('li')
+    li.textContent = todo
+    document.querySelector("#tasks").appendChild(li)
+}
+
+// Unable to parse .netrc file. Please run the 'learn whoami' command to ensure that you are authenticated. Use Ask a Question on Learn.co for additional help.
+
